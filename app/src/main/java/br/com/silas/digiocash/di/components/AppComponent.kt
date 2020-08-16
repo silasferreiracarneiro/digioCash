b4b1196182ae1ad241a1b44161ad421469363fcc
silasferreiracarneiro <silasferreira96@hotmail.com>
2020-08-16 16:53:48 -0300

    Removendo uma classe que não estava usando

--- app/src/main/java/br/com/silas/digiocash/di/components/AppComponent.kt
+++ app/src/main/java/br/com/silas/digiocash/di/components/AppComponent.kt
@@ -1,13 +1,12 @@
 package br.com.silas.digiocash.di.components
 
-import br.com.silas.digiocash.di.modules.ContextModule
 import br.com.silas.digiocash.di.modules.NetworkModule
 import br.com.silas.digiocash.ui.HomeActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [NetworkModule::class, ContextModule::class])
+@Component(modules = [NetworkModule::class])
 interface AppComponent {
     fun inject(activity: HomeActivity)
 }

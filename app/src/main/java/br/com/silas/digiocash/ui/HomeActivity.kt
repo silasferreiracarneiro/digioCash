a567be7503512df74e7a2f40fb3bf14a18b418dc
silasferreiracarneiro <silasferreira96@hotmail.com>
2020-08-16 10:54:38 -0300

    Criado uma instancia de viewmodel

--- app/src/main/java/br/com/silas/digiocash/ui/HomeActivity.kt
+++ app/src/main/java/br/com/silas/digiocash/ui/HomeActivity.kt
@@ -2,9 +2,14 @@ package br.com.silas.digiocash.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders.of
+import br.com.silas.digiocash.BaseApplication
 import br.com.silas.digiocash.R
-import dagger.android.support.DaggerAppCompatActivity
+import br.com.silas.digiocash.viewmodel.HomeViewModel
+import br.com.silas.digiocash.viewmodel.events.HomeViewModelEvent
+import br.com.silas.digiocash.viewmodel.events.HomeViewModelState
 import javax.inject.Inject
 
 
@@ -13,8 +18,38 @@ class HomeActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    lateinit var viewModel: HomeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        (application as BaseApplication).getAppComponent()?.inject(this)
+
+        bindViewModel()
+        bindObservable()
+        loadFiles()
+    }
+
+    private fun bindViewModel() {
+        viewModel = of(this, viewModelFactory).get(HomeViewModel::class.java)
+    }
+
+    private fun bindObservable() {
+        viewModel.viewEvent.observe(this, Observer {
+            when (it) {
+                is HomeViewModelEvent.ShowLoading -> null
+            }
+        })
+
+        viewModel.viewState.observe(this, Observer {
+            when (it) {
+                is HomeViewModelState.SucessCallApi -> null
+                is HomeViewModelState.ErrorCallApi -> null
+            }
+        })
+    }
+
+    private fun loadFiles() {
+        viewModel.loadFiles()
     }
 }
